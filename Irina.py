40bedaa71dcd649d1f131afc15b5c441e00edde8
Irina <ipotereanu@gmail.com>
2023-04-01 19:45:51 +0200

    continued on project

--- Irina.py
+++ Irina.py
@@ -1,54 +1,117 @@
+# SKETCHES 
+
 from argparse import ArgumentParser
 import sys
 from datetime import date 
 import time 
-users_register = {
+
+users_register ={
     "Federica": "TxelLci#2@8",
     "Irina_Bu": "Buhaniuc-irina",
-}
+       }
+purchases = [
+    {
+        "date": "01/04/23", # date of purchase 
+        "item": "name", #item-name str with at least 3 characters 
+        "cost": " 10",   # Cost of the item, including charges on delivery, should be float
+        "weight": "1",  # The weight of the item( should be a float, and in kg)
+        "quantity": "2", #The quantity purchased (should be an integer from 1 and above).
+    }
+]
+
+def password_validator(string: str)-> bool:
+    # will return True if the password is valid 
+    pass
+
+def phoneNumberValidator(phone_number: str)->bool:
+    # will return True if it is a valid german mobile number
+    pass
+
+def login(user_name: str, password: str)->bool:
+    #will return True if the username and password are correct
+    pass
+    
+def enter_purchase():
+    # The function will save the purchase details in a list of dictionaries
+    #at the and will print("Purchase saved.") 
+    pass
+
+def generate_report(): # will take information from dictionary 
+    #will generate the report and will print it 
+    pass 
+
+
+
+# registration 
 
 parser = ArgumentParser(description="Will take as argument user-name and password in order to have acces to amazon's datebase")
-
 parser.add_argument("user_name", type =str, help = "username")
-
 parser.add_argument("password", type =str, help = "password")
 
 args = parser.parse_args()
 
 user_name = args.user_name
 user_password = args.password
+
 # function to validate password 
-trys = 0
-turn_verifyinputs_on = True
-while turn_verifyinputs_on:
-    print("Verifying, please wait...\n")
-    time.sleep(2)
-    for user in users_register:
-        if user_name == user and users_register[user] == user_password:
-            print("Registration succesful!")
-            turn_verifyinputs_on = False
-            break
+# if password is valid => registration successful done,
+# Username and password are saved in users_register dictionary
+ 
+if password_validator(user_password):
+    users_register[user_name] = user_password
+
+
+
+phone_num_validator = False 
+while not phone_number_validator:
+    phone_number = input("Please enter phone number to continue: ")
+    # function for validating phone number, will return true or false 
+    phone_num_validator = phoneNumberValidator(phone_number)
+    
+
+
+#Login
+username = input("Username: ")
+password = input ("Password: ")
+if login(username, password):
+    print("-------------------------------------")
+    print("| Welcome to Amazon Expense Tracker! |")
+    print("-------------------------------------") 
+    
+    print(f"Hello, {username}! Welcome to the Amazon Expense Tracker!")
+    while option <3:
+        print(f"What would you like to do?")
+        print("1. Enter a purchase"
+            "2. Generate a report"
+            "3. Quit")
+        option = int(input("Enter your choice (1/2/3): "))
+        
+        if option == 1:
+            enter_purchase()
+        elif option == 2:
+            if option == 0:
+                print("Please enter at least one purchase")
+                break
+            generate_report()
+        elif option == 3:
+            print("Quitting program...")
+            time.sleep(2)
+            print(f"Thank you for your visit, {username}. Goodbye!")
+            sys.exit()     
         else:
-            if trys == 2:
-                print("Pleas register again!")
-                sys.exit(1)
-                turn_verifyinputs_on = False
-            print("Incorrect Username or Password. Please try again!")
-            print("Try again in 5 seconds")
-            trys += 1
-            time.sleep(1)
-            
-            user_password = input("Username: ")
-            user_password = input("Password:")
-            print("")
+            print("Please enter a valid option number.")   
+        
+    
 
     
 
-print("---------------------")
-print("| Welcome to Amazon! |")
-print("---------------------")
 
-user_phone_number = input("Please enter phone number to continue: ")
-# function to validate phone number 
+
+
+
+
+
+
+
 
 
