9b84bd9928e4e66ddd57ab78fe79b9a37e7d33ee
Irina <ipotereanu@gmail.com>
2023-04-06 12:09:28 +0200

    added Docstring

--- password.py
+++ password.py
@@ -5,6 +5,21 @@ import time
 
 
 def password_validator(password: str)->bool:
+    '''
+        This function will verify if the password is valid according to requirements.
+            Requirements:
+            - Should have at least one number.
+            - Should have at least one uppercase and one lowercase character.
+            - Should have at least one special symbol.
+            -Should be between 6 to 20 characters long.
+            -If the password is not valid the user will be ask to try again with a valid password, then exit the program.
+            
+            Parameters:
+                    password (str): String of characters (letters,symbols, numbers)
+
+            Returns:
+                    True or False, in case if password is valid or not.
+    '''   
     upper = bool(re.search("[A-Z]", password))
     lower = bool(re.search("[a-z]", password)) 
     numeric = bool(re.search("[0-9]", password))
