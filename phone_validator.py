7d8954af452f47bf442be67cb85d6cb465832883
Irina <ipotereanu@gmail.com>
2023-04-06 11:57:41 +0200

    Added DocString

--- phone_validator.py
+++ phone_validator.py
@@ -1,6 +1,15 @@
 import re
 
 def phoneNumberValidator() -> str:
+    '''
+        Returns the a valid german mobil number.
+
+            Parameters:
+                    no parameters
+
+            Returns:
+                    phone_number (str): String of numbers and character
+    '''
     while True:
         phone_number = input("Enter your phone number (ex. +49**********): ")
         if re.match(r"((\+49)|(0049))1(([5-7][0-9])|(9[0-9]))[0-9]{7,10}", phone_number):
